67f21086980260e9170d080b0f94c189809f2c0b
kaito121855 <kaito.121855@gmail.com>
2023-12-29 19:20:57 +0900

    assets-metadata-css/styling-tips

---
+++ src/components/Layout/layout.module.css.d.ts
@@ -0,0 +1,7 @@
+declare const styles: {
+  readonly "backToHome": string;
+  readonly "container": string;
+  readonly "header": string;
+};
+export = styles;
+
---
+++ src/styles/utils.module.css.d.ts
@@ -0,0 +1,14 @@
+declare const styles: {
+  readonly "borderCircle": string;
+  readonly "colorInherit": string;
+  readonly "heading2Xl": string;
+  readonly "headingLg": string;
+  readonly "headingMd": string;
+  readonly "headingXl": string;
+  readonly "lightText": string;
+  readonly "list": string;
+  readonly "listItem": string;
+  readonly "padding1px": string;
+};
+export = styles;
+
